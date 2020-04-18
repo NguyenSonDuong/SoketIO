07efb40ee7a2bd33485097631456b8f89b829538
NguyenSonDuong <hunterdemon9x99@gmail.com>
2020-04-18 13:55:05 +0700

    make it better

--- index.js
+++ index.js
@@ -5,17 +5,27 @@ app.set("view engine","ejs");
 app.set("views","./view");
 let server = require('http').Server(app);
 let io = require("socket.io")(server);
-
+let idUser = []
 io.on("connection",(socket)=>{
+    
     console.log("Connection: "+socket.id);
+    let name ;
+    socket.on("init",(data)=>{
+        idUser[data] = socket.id;
+        name = data;
+        socket.broadcast.emit("server",data+" đã đăng nhập")
+    });
     socket.on("cline",(data)=>{
-        console.log("Data: "+data);
-    })
+        console.log(idUser[data.to]);
+        io.to(idUser[data.to]).emit("server","Nickname: "+name+" Message: "+ data.message);
+        
+    });
     socket.on("disconnect",()=>{
-        console.log("Disconnect: "+socket.id);
-    })
+        
+    });
 
-})
+});
+io.emit() 
 
 app.get("/", (req,res)=>{ 
     res.render("home")
