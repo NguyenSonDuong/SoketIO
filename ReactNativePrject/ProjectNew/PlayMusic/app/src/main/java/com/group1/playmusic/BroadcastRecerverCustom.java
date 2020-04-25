437ff4f72a994621997c7c8db72993aa0182015b
NguyenSonDuong <hunterdemon9x99@gmail.com>
2020-04-25 14:00:08 +0700

    all

---
+++ ReactNativePrject/ProjectNew/PlayMusic/app/src/main/java/com/group1/playmusic/BroadcastRecerverCustom.java
@@ -0,0 +1,12 @@
+package com.group1.playmusic;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BroadcastRecerverCustom extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
